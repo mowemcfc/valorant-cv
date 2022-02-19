88cea751e3736f1e762c6f093f1a887f1fa636fc
mowemcfc <jcartermcfc@gmail.com>
2022-02-19 20:58:57 +1100

    Attach s3 r/w policy to Sagemaker

--- modules/roles.tf
+++ modules/roles.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "carter-dev-role" {
-  name = "carter-dev"
+  name = "sagemaker-instance-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -12,6 +12,32 @@ resource "aws_iam_role" "carter-dev-role" {
           Service = "sagemaker.amazonaws.com"
         }
       },
+
     ]
   })
+}
+
+
+resource "aws_iam_policy" "sagemaker_s3_rw_policy" {
+  name = "sagemaker_s3_rw_policy"
+  description = "Allows sagemaker read+write access to all s3"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action" : [
+          "s3:*"
+        ],
+        Effect: "Allow",
+        Resource: "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "sagemaker_rw_access_attachment" {
+  name = "sagemaker_rw_access_attachment"
+  roles = [aws_iam_role.carter-dev-role.name]
+  policy_arn = aws_iam_policy.sagemaker_s3_rw_policy.arn
 }
